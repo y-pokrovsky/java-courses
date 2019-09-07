94e29b7b29842a57880cfbbfa3f12d97b1a9d093
Yura Pokrovskiy <y-pokrovsky@yandex.ru>
2019-09-08 00:08:23 +0300

    user input

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+
+public class Calculator {
+	
+	private int result;
+	
+	public void add(int ... params) {
+		for (Integer param : params) {
+			this.result += param;
+		}
+	}
+	
+	public int getResult() {
+		return this.result;
+	}
+	
+	public void cleanResult() {
+		this.result = 0;
+	}
+}
